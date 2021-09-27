547987b6bdc27d8f42923c22dfe0c1e13ef986cd
Shashank Bengaluru Srinivasa <sbengal2@binghamton.edu>
2021-09-27 17:28:25 -0400

    added functionality to handle variables

--- shell.py
+++ shell.py
@@ -3,6 +3,7 @@ import mocha
 while True:
     text = input('mocha > ')
     result,error = mocha.run('<stdin>',text)
-
-    if error: print(error.as_string())
-    else: print(result)
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
